0bac25e221ec7506367ac1f98c6a877a084e9251
Jesse Mcnamara <xxologistxx@gmail.com>
2016-05-04 12:42:38 +1000

    created test project

---
+++ OpenCvTest/OpenCvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCvTest
+//
+//  Created by Jesse on 4/05/2016.
+//  Copyright © 2016 DragonDinosaurLPLUCIEN. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
